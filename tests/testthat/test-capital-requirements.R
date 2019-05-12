d74225431bc4d5f51d4bed9658c564bebd4061dd
Dan Jordan <dandermotj@gmail.com>
2019-05-12 15:19:31 +0100

    Actual reasonbable tests for IRB risk weights

--- tests/testthat/test-capital-requirements.R
+++ tests/testthat/test-capital-requirements.R
@@ -1,22 +1,49 @@
-context("Capital Requirements")
-library(basel)
+context("test-capital-calcs")
 
 EAD <- 100
-PD <- 0.05
+PD  <- 0.05
 LGD <- 0.8
-M <- 2.5
 
-# Testing that capital_adequacy runs for:
-#  * corporate
-#  * SME
-#  * revolving
-#  * mortgage
-#  * other
+test_that("capital_requirement is correct", {
 
-expect_gt(capital_requirement(PD, LGD, M, portfolio = "corporate"), 0)
-expect_gt(capital_requirement(PD, LGD, M, portfolio = "SME"), 0)
-expect_gt(capital_requirement(PD, LGD, M, portfolio = "revolving"), 0)
-expect_gt(capital_requirement(PD, LGD, M, portfolio = "mortgage"), 0)
-# Leave until worked this one out
-# expect_gt(capital_requirement(PD, LGD, M, portfolio = "other"), 0)
-expect_error(capital_requirement(PD, LGD, M, portfolio = "gobbledygook"))
+  corp  <- capital_requirement("corporate", PD, LGD)
+  sme   <- capital_requirement("SME", PD, LGD)
+  rev   <- capital_requirement("revolving", PD, LGD)
+  mort  <- capital_requirement("mortgage", PD, LGD)
+  other <- capital_requirement("other", PD, LGD)
+
+  expect_equal(corp, 0.01702401)
+  expect_equal(sme, 0.01702401)
+  expect_equal(rev, 0.007065785)
+  expect_equal(mort, 0.01913075)
+  expect_equal(other, 0.008572082)
+  expect_error(capital_requirement(portfolio = "gobbledygook", PD, LGD, M))
+
+})
+
+
+test_that("RWA is correct", {
+
+  expect_equal(rwa(0.01, 100), 12.5)
+
+})
+
+test_that("expected loss fn works", {
+
+  expect_equal(expected_loss(EAD, PD, LGD), 4)
+
+})
+
+test_that("asset correlations are correct", {
+
+  corp  <- asset_corr("corporate", c(0, 1))
+  rev   <- asset_corr("revolving")
+  mort  <- asset_corr("mortgage")
+  other <- asset_corr("other", c(0, 1))
+
+  expect_equal(corp, c(0.24, 0.12))
+  expect_equal(rev, 0.04)
+  expect_equal(mort, 0.15)
+  expect_equal(other, c(0.16, 0.03))
+
+})
